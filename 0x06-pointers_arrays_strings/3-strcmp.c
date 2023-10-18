7fe094b59ed9bc120c5e06f8a7070479c611b4bf
Vincent Karani <karanivincent86@gmail.com>
2023-10-18 12:42:39 -0700

    pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncat - concatenate strings, defining the size of second string.
+ * @dest: string with concatenation
+ * @src: string to be concatenated
+ * @n: size of second string
+ * Return: Always 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int l, w;
+
+	l = 0;
+	w = 0;
+
+	while (*(dest + l) != '\0')
+		l++;
+
+	while (*(src + w) != '\0' && l < 97 && w < n)
+	{
+		*(dest + l) = *(src + w);
+		l++;
+		w++;
+	}
+	*(dest + l) = '\0';
+	return (dest);
+}
+
