6ef83faeee27699435cba2d1fe562910c66020d2
Vincent Karani <karanivincent86@gmail.com>
2023-10-18 13:40:59 -0700

    pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes chars in a string following a separator
+ * @c: character string pointer
+ * Return: char pointer
+ */
+char *cap_string(char *c)
+{
+	int m = 0, n,
+	    sep[] = {32, '\t', 11, '\n', 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	if (c[0] > 96 && c[0] < 123)
+		c[0] -= 32;
+	while (c[m] != '\0')
+	{
+		if (c[m] > 96 && c[m] < 123)
+		{
+			n = 0;
+			while (m < 14)
+			{
+				if (c[m - 1] == sep[j])
+				{
+					c[m] -= 32;
+					break;
+				}
+				n++;
+			}
+		}
+		m++;
+	}
+	return (c);
+}
