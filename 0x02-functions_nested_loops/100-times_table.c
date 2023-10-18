3ea443911f1303e907c46077409ab92e6f7e0d8d
Vincent Karani <karanivincent86@gmail.com>
2023-10-18 07:07:02 -0700

    C-Functions, nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,39 +1,49 @@
 #include "main.h"
+
 /**
  * print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
  */
-
 void print_times_table(int n)
-{
-	int num, mult, prod;
+{int q, r, s;
 
 	if (n >= 0 && n <= 15)
 	{
-		for (num = 0; num <= n; mult++)
+		for (q = 0; q <= n; q++)
 		{
-			_putchar('0');
-			for (mult = 1; mult <= n; mult++)
+			for (r = 0; r <= n; r++)
 			{
-				_putchar(',');
-				_putchar(' ');
-				prod = num * mult;
-				if (prod <= 99)
-					_putchar(' ');
-				if (prod <= 0)
-					_putchar(' ');
-				if (prod >= 100)
+				s = r * q;
+				if (r == 0)
 				{
-					_putchar((prod / 100) + '0');
-					_putchar((prod / 10) % 10 + '0');
-				}
-				else if (prod <= 99 && prod >= 10)
+					_putchar(s + '0');
+				} 
+				else if (s < 10 && r != 0)
 				{
-					_putchar((prod / 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(s + '0');
 				}
-				_putchar((prod % 10) + '0');
+				else if (s >= 10 && s < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((s / 10) + '0');
+					_putchar((s % 10) + '0');
+				}
+				else if (s >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((s / 100) + '0');
+					_putchar(((s / 10) % 10) + '0');
+					_putchar((s % 10) + '0');
+				}
 			}
-			_putchar('\n');
+			_putchar('\n');	
 		}
 	}
 }
