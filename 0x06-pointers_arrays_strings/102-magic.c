4e582fccedc42393653a69e6f2fb5017f94ffa35
Vincent Karani <karanivincent86@gmail.com>
2023-10-18 22:50:10 -0700

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,23 +1,14 @@
 #include <stdio.h>
-
+/**
+ * main - Add one line to this code.
+ * Description: program prints a[2] = 98,
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int n;
-	int a[5];
-	int *p;
+	int a[] = {45, 67, 12, 89, 43};
+	int *p = a;
 
-	a[2] = 1024;
-	p = &n;
-	/*
-	 * write your line of code here...
-	 * Remember:
-	 * - you are not allowed to use a
-	 * - you are not allowed to modify p
-	 * - only one statement
-	 * - you are not allowed to code anything else than this line of code
-	 */
-	*(p + 5) = 98;
-	/* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
-	return (0);
+	p = p + 2;
+	printf("a[2] = %d\n", *(p));
 }
