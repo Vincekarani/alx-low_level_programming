855aff4470748af952ea7ee1bc02b74c1df4ffc7
Vincent Karani <karanivincent86@gmail.com>
2023-10-19 10:36:33 -0700

    pointers, arrays, strings

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -8,9 +8,10 @@
  */
 void print_buffer(char *b, int size)
 {
-	int o, j, i;
+	int o, r, q;
 
 	o = 0;
+
 	if (size <= 0)
 	{
 		printf("\n");
@@ -18,22 +19,22 @@ void print_buffer(char *b, int size)
 	}
 	while (o < size)
 	{
-		j = size - o < 10 ? size - o : 10;
+		r = size - o < 10 ? size - o : 10;
 		printf("%08x: ", o);
-		for (i = 0; i < 10; i++)
+		for (q = 0; q < 10; q++)
 		{
-			if (i < j)
-				printf("%02x", *(b + o + i));
+			if (q < r)
+				printf("%02x", *(b + o + q));
 			else
 				printf(" ");
-			if (i % 2)
+			if (q % 2)
 			{
 				printf(" ");
 			}
 		}
-		for (i = 0; i < j; i++)
+		for (q = 0; q < r; q++)
 		{
-			int c = *(b + o + i);
+			int c = *(b + o + q);
 
 			if (c < 32 || c > 132)
 			{
