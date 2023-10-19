fc2e502d58bb0dcee84d703dd5e31ea70ac8de22
Vincent Karani <karanivincent86@gmail.com>
2023-10-19 10:45:40 -0700

    pointers, arrays, strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,27 +1,20 @@
 #include <stdio.h>
 /**
- * main -Add one line to this code
  * Description: program prints a[2] = 98
- * Return: Always (0).
  */
 int main(void)
-
 {
-	int n;
-	int a[5];
-	int *p;
+	int n, a[5], *p;
 
 	a[2] = 1024;
 	p = &n;
-
-	/*
-	 *
+	/**
 	 * write your line of code here...
 	 * Remember:
 	 * - you are not allowed to use a
-	 *   - you are not allowed to modify p
-	 *   - only one statement
-	 *   - you are not allowed to code anything else than this line of code
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
 	 */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
