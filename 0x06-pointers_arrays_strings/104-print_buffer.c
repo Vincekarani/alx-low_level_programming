9b8244167091ab15b7c6b63ea1bf0c6f88c0b087
Vincent Karani <karanivincent86@gmail.com>
2023-10-20 13:04:59 -0700

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,19 +1,16 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * print_buffer - prints buffer
  * @b: buffer
  * @size: size
  * Return: void
  */
-
 void print_buffer(char *b, int size)
 {
 	int o;
 	int r;
 	int q;
-
 	o = 0;
 	if (size <= 0)
 	{
@@ -22,12 +19,12 @@ void print_buffer(char *b, int size)
 	}
 	while (o < size)
 	{
-		q = size - o < 10 ? size - o : 10;
+		r = size - o < 10 ? size - o : 10;
 		printf("%08x:", o);
 		for (q = 0; q < 10; q++)
 		{
 			if (q < r)
-			printf("%02x", *(b + o + q));
+				printf("%02x", *(b + o + q));
 			else
 				printf(" ");
 			if (q % 2)
@@ -38,7 +35,6 @@ void print_buffer(char *b, int size)
 		for (q = 0; q < r; q++)
 		{
 			int c = *(b + o + q);
-
 			if (c < 32 || c > 132)
 			{
 				c = '.';
