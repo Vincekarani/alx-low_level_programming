627e1023fab31c53b2330ebd053a5337689d70f3
Vincent Karani <karanivincent86@gmail.com>
2023-10-21 00:12:01 -0700

    pointer_arrays_strings

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,58 +1,55 @@
-#include "main.h"
 #include <stdio.h>
-/**
- * print_line - prints a s bytes of a buffer
- * @c: buffer to print
- * @s: bytes of buffer to print
- * @l: line of buffer to print
- * Return: void
- */
+#include <ctype.h>
 
-void print_line(char *c, int s, int l)
-{
-	int j, k;
-
-	for (j = 0; j <= 9; j++)
-	{
-		if (j <= s)
-			printf("%02x", c[l * 10 + j]);
-		else
-			printf(" ");
-		if (j % 2)
-			putchar(' ');
-	}
-	for (k = 0; k <= s; k++)
-	{
-		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
-			putchar(c[l * 10 + k]);
-		else
-			putchar('.');
-	}
-}
-
-/**
- * print_buffer - prints a buffer
- * @b: buffer to print
- * @size: size of buffer
- * Return: void
- */
 void print_buffer(char *b, int size)
 {
+	if (size <= 0)
+	{
+	printf("\n");
+	return;
+	}
 	int i;
 
-	for (i = 0; i <= (size - 1) / 10 && size; i++)
+	for (i = 0; i < size; i++)
 	{
-		printf("%08x: ", i * 10);
-		if (i < size / 10)
+	/*
+	* Print the position in hexadecimal (8 chars)
+	*/
+	printf("%08X ", i);
+	 int j;
+
+	for (j = 0; j < 10 && i < size; j++, i++)
+	}
+	{
+	/*
+	* Print the hexadecimal content of the buffer, 2 bytes at a time
+	*/
+	printf("%02X ", (unsigned char)b[i]);
+	/*
+	* Print the the buffer, replace non-printable chars with '.'
+	*/
+		if (isprint(b[i]))
 		{
-			print_line(b, 9, i);
+		printf("%c", b[i]);
 		}
 		else
 		{
-			print_line(b, size % 10 - 1, i);
+		printf(".");
 		}
-		putchar('\n');
 	}
-	if (size == 0)
-		putchar('\n');
+		{
+		printf("\n");
+		}
+	{
+
+	{
+	char buffer[] = "This is a test buffer to print.";
+	int size = sizeof(buffer) - 1;
+
+	/*
+	* Excluding the null terminato
+	*/
+	print_buffer(buffer, size);
+	return (0);
+	}
 }
