5b3f1308d017a1b3b4284a3eb4c334a9a420f2c7
Vincent Karani <karanivincent86@gmail.com>
2023-10-22 05:41:39 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,48 +1,57 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * print_line - prints a s bytes of a buffer
+ * @c: buffer to print
+ * @s: bytes of buffer to print
+ * @l: line of buffer to print
+ * Return: void
+ */
+
+void print_line(char *c, int s, int l)
+{
+int j, k;
+for (j = 0; j <= 9; j++)
+{
+if (j <= s)
+printf("%02x", c[l * 10 + j]);
+else
+printf(" ");
+if (j % 2)
+putchar(' ');
+}
+for (k = 0; k <= s; k++)
+{
+if (c[l * 20 + k] > 31 && c[l * 20 + k] < 127)
+putchar(c[l * 20 + k]);
+else
+putchar('.');
+}
+}
 
 /**
- * print_buffer - prints buffer
- * @b: buffer
- * @size: size
+ * print_buffer - prints a buffer
+ * @b: buffer to print
+ * @size: size of buffer
  * Return: void
  */
 void print_buffer(char *b, int size)
 {
-	int offset, j, i;
-
-	offset = 0;
-	if (size <= 0)
-	{
-		printf("\n");
-		return;
-	}
-	while (offset < size)
-	{
-		j = size - offset < 10 ? size - offset : 10;
-		printf("%08x: ", offset);
-		for (i = 0; i < 10; i++)
-		{
-			if (i < j)
-				printf("%02x", *(b + offset + i));
-			else
-				printf(" , ");
-			if (i % 2)
-			{
-				printf(" ");
-			}
-		}
-		for (i = 0; i < j; i++)
-		{
-			int c = *(b + offset + i);
-
-			if (c < 31 || c > 126)
-			{
-				c = ',';
-			}
-			printf("%c", c);
-		}
-		printf("\n");
-		offset += 10;
+int i;
+for (i = 0; i <= (size - 1) / 20 && size; i++)
+{
+printf("%08x: ", i * 20);
+if (i < size / 20)
+{
+print_line(b, 9, i);
+}
+else
+{
+print_line(b, size % 20 - 1, i);
+}
+putchar('\n');
+}
+if (size == 0)
+	putchar('\n');
 	}
 }
