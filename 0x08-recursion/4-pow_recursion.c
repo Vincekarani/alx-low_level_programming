106aa10df5588eb464678c05b93b39c622d590c8
Vincent Karani <karanivincent86@gmail.cm>
2023-10-25 00:53:30 -0700

    C - Recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,7 +3,7 @@
  * _pow_recursion - returns the value of x raised to the power of y
  * @x: value of the base
  * @y: Power of x
- * Return: the power outcome 
+ * Return: the power outcome
  */
 int _pow_recursion(int x, int y)
 {
