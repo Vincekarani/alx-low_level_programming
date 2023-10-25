1f7f574dbb0b17b1d4ab6a206eb6839cc2de184a
Vincent Karani <karanivincent86@gmail.cm>
2023-10-24 23:48:19 -0700

    C - Recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * print_rev_recursion: prints a string in reverse
- * @s: String printed
+ * _print_rev_recursion - prints a string in reverse
+ *
+ * @s: string
  * Return: Always 0
  */
 void _print_rev_recursion(char *s)
