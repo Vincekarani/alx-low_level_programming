62661df3d6418a5b01812ccce3cb5c33ed3bcf00
Vincent Karani <karanivincent86@gmail.cm>
2023-10-25 00:40:11 -0700

    C - Recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _pow_recursion - returns the value of x raised to the power of y.
- *
+ * _pow_recursion - returns the value of x raised to the power of y
  * @x: The base
  * @y: The index
+ *
  * Retutn: (-1)
  */
 int _pow_recursion(int x, int y)
