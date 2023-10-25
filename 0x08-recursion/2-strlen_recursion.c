a3e5ccbf78dac23171bfd017fc50170c4a78119e
Vincent Karani <karanivincent86@gmail.cm>
2023-10-24 23:56:05 -0700

    C - Recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion: returns the length of a string.
+ *
+ * @s: String
+ * Return: Always 0
+ */
+int _strlen_recursion(char *s)
+{
+	int len;
+
+	if (*s == '\0')
+		return (0);
+	len = _strlen_recusion(s + 1);
+	return (len + 1);
+}
+	
