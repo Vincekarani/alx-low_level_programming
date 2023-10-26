1d509e1bad3257a628cc6cb76c9742fdfb6ea14a
Vincent Karani <karanivincent86@gmail.cm>
2023-10-25 18:19:35 -0700

    C - Pointers, arrays and strings

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -16,16 +16,16 @@ void print_line(char *c, int s, int b)
 	for (j = 0; j <= 10; j++)
 	{
 		if (j <= s)
-			printf(" %04x ", c[b * 10 + j]);
+			printf("%02x", c(b * 10 + ));
 		else
 			printf(" ");
-		if (j % 4)
+		if (j % 2)
 			putchar(' ');
 	}
 	for (k = 0; k <= s; k++)
 	{
-		if (c[b * 10 + k] > 31 && c[b * 10 + k] < 127)
-			putchar(c[b * 10 + k]);
+		if (c(b * 10 + k) > 31 && c(b * 10 + k) < 127)
+			putchar(c(b * 10 + k));
 		else
 			putchar('.');
 	}
@@ -44,7 +44,7 @@ void print_buffer(char *b, int size)
 
 	for (i = 0; i <= (size - 1) / 10 && size; i++)
 	{
-		printf(" %08x: ", i * 10);
+		printf("%08x:", i * 10);
 		if (i < size / 10)
 		{
 			print_line(b, 9, i);
