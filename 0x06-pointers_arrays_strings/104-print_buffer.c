da945cb5f114d1b28bfeabc7f287c838a6068bd6
Vincent Karani <karanivincent86@gmail.cm>
2023-10-25 23:27:21 -0700

    C - Pointers, arrays and strings

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * print_buffer - prints a buffer
  * @b: the buffer to print
@@ -11,30 +10,31 @@ void print_buffer(char *b, int size)
 {
 int i;
 
-for (i = 0; i <= (size - 1) / 20 && size; i++)
+for (i = 0; i <= (size - 1) / 10 && size; i++)
 {
-printf("%08x: ", i * 20);
-if (i < size / 20)
+printf("%08x: ", i * 10);
+if (i < size / 10)
 {
 print_line(b, 9, i);
 }
 else
 {
-print_line(b, size % 20 - 1, i);
+print_line(b, size % 10 - 1, i);
 }
 putchar('\n');
 }
 if (size == 0)
 	putchar('\n');
 	}
-
 /**
  * print_line - prints a s bytes of a buffer
- * @c: buffer to print
- * @s: bytes of buffer to print
+ * @c: the buffer to print
+ * @s: the bytes of buffer to print
  * @l: the line of buffer to print
+ *
  * Return: void
  */
+
 void print_line(char *c, int s, int l)
 {
 int j, k;
@@ -42,7 +42,7 @@ int j, k;
 for (j = 0; j <= 9; j++)
 {
 if (j <= s)
-printf("%02x", c[l * 20 + j]);
+printf("%02x", c[l * 10 + j]);
 else
 printf(" ");
 if (j % 2)
@@ -50,8 +50,8 @@ putchar(' ');
 }
 for (k = 0; k <= s; k++)
 {
-if (c[l * 20 + k] > 31 && c[l * 20 + k] < 127)
-putchar(c[l * 20 + k]);
+if (c[l * 10 + k] >= 32 && c[l * 10 + k] <= 126)
+putchar(c[l * 10 + k]);
 else
 putchar('.');
 }
