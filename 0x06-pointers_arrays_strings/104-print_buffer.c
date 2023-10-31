22fc5f7c00db23eb2af986a27278c48c97a8a049
Vincent Karani <karanivincent86@gmail.com>
2023-10-31 13:36:49 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -14,14 +14,14 @@ void print_buffer(char *b, int size)
 
 	for (h = 0; h <= (size - 1) / 10 && size; h++)
 	{
-		printf("%08x:", h * 10);
+		printf("%08x :", h * 10);
 		if (h < size / 10)
 		{
 			print_line(b, 9, h);
 		}
 		else
 		{
-			print_line(b, size%10 + 1, h);
+			print_line(b, size % 10 - 1, h);
 		}
 		putchar('\n');
 	}
