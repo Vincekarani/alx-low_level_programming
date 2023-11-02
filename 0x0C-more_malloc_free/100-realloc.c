14a970265911a1a978f5132d5cdf2e95f8b2c176
Vincent Karani <karanivincent86@gmail.com>
2023-11-02 02:34:49 -0700

    0x0C. C - More malloc, free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,7 +13,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *ptr1;
 	char *old_ptr;
-	unsigned int i;
+	unsigned int j;
 
 	if (new_size == old_size)
 		return (ptr);
