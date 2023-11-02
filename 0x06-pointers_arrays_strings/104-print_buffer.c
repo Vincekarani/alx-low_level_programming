398e0fe1c641ecebe01a21cbe51b0b4ba9977cb8
Vincent Karani <karanivincent86@gmail.com>
2023-11-02 10:02:10 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,50 +1,77 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * isPrintableASCII - determines if n is a printable ASCII char
+ * @n: integer
+ * Return: 1 if true, 0 if false
+ */
+int isPrintableASCII(int n)
+{
+		return (n >= 32 && n <= 126);
+}
+/**
+ * printHexes - print hexadecimal values for string b in formatted form
+ * @b: print string
+ * @start: position to start
+ * @end: position to end
+ */
+void printHexes(char *b, int start, int end)
+{
+	int k = 0;
+
+	while (k < 10)
+	{
+		if (k < end)
+			printf("%02x", *(b + start + k));
+		else
+			printf(" ");
+		if (k % 2)
+			printf(" ");
+		k++;
+	}
+}
 
 /**
- * print_buffer - prints buffer
- * @b: buffer
- * @size: size
- * Return: void
+ * printASCII - print ascii values for str b,
+ * replace nonprintable chars with '.'
+ * @b: string to be printed
+ * @start: to start position
+ * @end: position to end
  */
+void printASCII(char *b, int start, int end)
+{
+	int h, k = 0;
 
+	while (k < end)
+	{
+		h = *(b + k + start);
+		if (!isPrintableASCII(h))
+			h = 46;
+		printf("%c", h);
+		k++;
+	}
+}
+
+/**
+ * print_buffer - prints a buffer
+ * @b: the string
+ * @size: the buffer size
+ */
 void print_buffer(char *b, int size)
 {
-	int o, j, i;
+	int start, end;
 
-	o = 0;
-
-	if (size <= 0)
+	if (size > 0)
 	{
-		printf("\n");
-		return;
-	}
-	while (o < size)
-	{
-		j = size - o < 10 ? size - o : 10;
-		printf("%08x: ", o);
-		for (i = 0; i < 10; i++)
+		for (start = 0; start < size; start += 10)
 		{
-			if (i < j)
-				printf("%02x", *(b + o + i));
-			else
-				printf(" ");
-			if (i % 2)
-			{
-				printf(" ");
-			}
+			end = (size - start < 10) ? size - start : 10;
+			printf("%08x: ", start);
+			printHexes(b, start, end);
+			printASCII(b, start, end);
+			printf("\n");
 		}
-		for (i = 0; i < j; i++)
-		{
-			int c = *(b + o + i);
-
-			if (c < 32 || c > 132)
-			{
-				c = '.';
-			}
-			printf("%c", c);
-		}
-		printf("\n");
-		o += 10;
 	}
+	else
+		printf("\n");
 }
