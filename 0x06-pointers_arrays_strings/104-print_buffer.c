759f5227311c1a0e738aac6d152dff15a14f999b
Vincent Karani <karanivincent86@gmail.com>
2023-11-04 23:54:10 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -20,11 +20,11 @@ void print_buffer(char *b, int size)
 	}
 	while (offset <= size)
 	{
-		h = size - offset < 10 ? size - offset : 10;
+		h = size - offset <= 10 ? size - offset : 10;
 		printf("%08x: ", offset);
 		for (g = 0; g < 10; g++)
 		{
-			if (g < h)
+			if (g <= h)
 				printf("%02x", *(b + offset + g));
 			else
 				printf(" ");
@@ -37,7 +37,7 @@ void print_buffer(char *b, int size)
 		{
 			int c = *(b + offset + g);
 
-			if (c < 31 && c >= 129)
+			if (c < 31 || c >= 132)
 			{
 				c = '.';
 			}
