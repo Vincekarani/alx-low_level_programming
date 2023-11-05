e0de674b3d9876de31fed9499b17f0220df31117
Vincent Karani <karanivincent86@gmail.com>
2023-11-04 18:31:45 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,15 +1,16 @@
-
 #include "main.h"
 #include <stdio.h>
-/** 
+
+/**
  * print_buffer - prints buffer
- * @b: the buffer to print 
- * @size: the size to print 
- * Return: void 
+ * @b: the buffer
+ * @size: the size
+ * Return: void
  */
+
 void print_buffer(char *b, int size)
 {
-	int offset, k, i;
+	int offset, h, g;
 
 	offset = 0;
 	if (size <= 0)
@@ -19,28 +20,28 @@ void print_buffer(char *b, int size)
 	}
 	while (offset < size)
 	{
-		k = size - offset < 10 ? size - offset: 10;
-		printf("%08x:" , offset);
-		for (i = 0; i < 10	; i++)
+		h = size - offset < 10 ? size - offset : 10;
+		printf("%08x: ", offset);
+		for (g = 0; g < 10; g++)
 		{
-			if (i < k)
-				printf("%02x" , *(b + offset + i));
+			if (g < h)
+				printf("%02x", *(b + offset + g));
 			else
-				printf(" ");
-			if (i % 2)
+				printf("  ");
+			if (g % 2)
 			{
-				printf(" ");
+				printf("  ");
 			}
 		}
-		for (i = 0; i < k; i++)
+		for (g = 0; g < h; g++)
 		{
-			int c = *(b + offset + i);
+			int c = *(b + offset + g);
 
-			if (c <= 31 || c >= 127)
+			if (c < 32 || c > 127)
 			{
 				c = '.';
 			}
-			printf("%c\n" , c);
+			printf("%c", c);
 		}
 		printf("\n");
 		offset += 10;
