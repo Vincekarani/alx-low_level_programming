a6aba5bcae2fbaeaa94e2a787813a100019ca17f
Vincent Karani <karanivincent86@gmail.com>
2023-11-08 11:06:37 -0800

    0x0F. C - Function pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "3-calc.h"
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+/**
+ * op_add - Returns the sum of two numbers.
+ * @a: number 1.
+ * @b: number 2.
+ *
+ * Return: The addition 
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - Returns the difference of two numbers.
+ * @a: number 1.
+ * @b: number 2.
+ *
+ * Return: The subtraction .
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - Returns the product of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The multiplication 
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Returns the division of two numbers.
+ * @a: number 1.
+ * @b: number 2.
+ *
+ * Return: The quotient of a and b.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - Returns the remainder of the division of two numbers.
+ * @a: number 1.
+ * @b: number 2.
+ *
+ * Return: The remainder of a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
