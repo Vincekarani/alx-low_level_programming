b92ff783c084348059f3c7ca29a249fb073d1097
Vincent Karani <karanivincent86@gmail.com>
2023-11-09 07:06:23 -0800

    0x10. C - Variadic functions

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - returns the sum of all its parameters.
+ * @n: infinite number of parameters
+ * @separator: separates argumenta
+ *
+ * Return: NULL
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int k;
+	int numb;
+	va_list infinite;
+
+	for (k = 0; k < n; k++)
+	{
+		numb = va_arg(infinite, int);
+		printf("%d", numb);
+
+	if ((k != n - 1) && separator != NULL)
+		printf("%s", separator);
+	}
+	va_end(infinite);
+	printf("\n");
+}
