4ee22b1d17cff517f6a2a6b85d003a64bd545ac2
root <karanivincent86@gmail.com>
2023-11-23 11:13:43 -0800

    0x14. C - Bit manipulation

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,13 +12,13 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int i, num = 0;
-	unsigned long int current;
-	unsigned long int exclusive = n ^ m;
+	unsigned long int c;
+	unsigned long int x = n ^ m;
 
-	for (i = 81; i >= 0; i--)
+	for (i = 63; i >= 0; i--)
 	{
-		current = exclusive >> i;
-		if (current & i)
+		c = x >> i;
+		if (c & i)
 			num++;
 	}
 	return (num);
