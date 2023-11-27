d520cae5c07b5f14c0415789954139ec96d2ec6f
root <karanivincent86@gmail.com>
2023-11-27 13:30:35 -0800

    0x15. C - File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (!text_content)
 		text_content = "";
-	for (letters = 0; text_content[letters]; letters++);
+	for (letters = 0; text_content[letters]; letters++)
 	wr = write(f, text_content, letters);
 	if (wr == -1)
 		return (-1);
