5a9eef6e59254a7cc781b84ae1625e2fae087a13
root <karanivincent86@gmail.com>
2023-11-27 12:32:57 -0800

    0x15. C - File I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,7 +17,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (!filename)
 		return (0);
-	fd = open(filename, O_RDONLY);
+	f = open(filename, O_RDONLY);
 	if (f == -1)
 		return (0);
 	buff = malloc(sizeof(char) * (letters));
