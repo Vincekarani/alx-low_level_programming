38cefb45005d75866f0c68ef1926cafe112bb7c6
root <karanivincent86@gmail.com>
2023-11-27 12:42:07 -0800

    0x15. C - File I/O

---
+++ 0x15-file_io/2-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * create_file - a function that creates a file.
+ * @filename: name of the file to create
+ * and text_content is a NULL terminated
+ * string to write to the file.
+ * @text_content: a NULL terminated
+ * string to write to the file
+ * Return: 1 on success. -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int f;
+	int letters;
+	int wr;
+
+	if (!filename)
+		return (-1);
+	f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (f == -1)
+		return (-1);
+	if (!text_content)
+		text_content = "";
+	for (letters = 0; text_content[nletters]; letters++);
+	wr = write(f, text_content, letters);
+	if (wr == -1)
+		return (-1);
+	close(f);
+	return (1);
+}	
