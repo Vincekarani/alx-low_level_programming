09666b4d591c9bc9df69c58defb0a43784040223
root <karanivincent86@gmail.com>
2023-11-27 22:08:29 -0800

    0x15. C - File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,13 +15,18 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+
 	f = open(filename, (O_CREAT | O_TRUNC) | O_WRONLY, 0600);
+
 	if (f == -1)
 		return (-1);
+
 	while (text_content[len] != '\0')
 		len++;
+
 	ch = write(f, text_content, len);
 	close(f);
+
 	if (ch != -1)
 		return (1);
 	else
