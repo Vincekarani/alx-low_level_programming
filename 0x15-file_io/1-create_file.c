1280c29da1fb043c6689b710a20cb7f700b31868
root <karanivincent86@gmail.com>
2023-11-27 20:55:04 -0800

    0x15. C - File I/O C Syscall

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	if (f == -1)
 		return (-1);
 	if (!text_content)
-		text_content ="";
+		text_content = " ";
 	for (letters = 0; text_content[letters]; letters++)
 	wr = write(f, text_content, letters);
 	if (wr == -1)
