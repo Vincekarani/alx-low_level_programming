2ce720e170595c6d9c37695b65bf9b56392934b0
root <karanivincent86@gmail.com>
2023-11-27 22:34:43 -0800

    0x15. C - File I/O

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,7 +20,8 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content)
 	{
-		for (letters = 0; text_content[letters]; letters++);
+		for (letters = 0; text_content[letters]; letters++)
+			;
 		wr = write(f, text_content, letters);
 		if (wr == -1)
 			return (-1);
